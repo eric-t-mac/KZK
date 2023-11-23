9200078137f85a2f38423ee035a3b26498b05e42
v_zekaikang <v_zekaikang@tencent.com>
2023-11-23 10:11:02 +0800

    refresh:父子组件通信

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import React from 'react'
 // import TestCondition from './views/study/TestCondition'
 // import TestList from './views/study/TestList'
 // import TestForm from './views/study/TestForm'
-import TestLife from './views/study/TestLife'
+// import TestLife from './views/study/TestLife'
+import TestLang from './views/study/TestLang'
 
 // const ele = <h1>Hello React 2009</h1>
 
 // 类组件，组件定义
 class App extends React.Component {
   render() {
-    return <TestLife />
+    return <TestLang />
   }
 }
 
--- src/views/study/TestLife.js
+++ src/views/study/TestLife.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// 三个阶段：装载阶段（3）、更新阶段（2）、销毁阶段（1）
+
 // 当React组件初始化时，JSX的DOM结构会在内存里创建成虚拟DOM，就是一个JSON文件，JSON文件会详细的描述这些节点
 // 当setState的时候，同样会在内存中生成一个新的虚拟DOM，会触发Diff运算，将初始的虚拟DOM和当前新的虚拟DOM进行比对，
 // 找出最小差异，并将最小差异相关的节点（也就是脏节点）进行真实DOM更新
@@ -31,6 +33,7 @@ export default class TestLife extends React.Component {
     // 这是一个开关，用于控制是否更新
     // 询问React，我该更新界面？
     // 这个生命周期，用于性能优化，我们可以精确地控制某些state变量发生变化时不更新视图
+    // 实际工作中很少用，一般函数式组件（PureComponent）作为替代解决
     shouldComponentUpdate(nextProps, nextState) {
         console.log('---------shouldComponentUpdate');
         console.log('开关打开state', nextState);
@@ -41,13 +44,16 @@ export default class TestLife extends React.Component {
             return true
         }
     }
-
     // 相当于Vue中的updated()
+    // 表示DOM已经更新完成
+    // 在该生命周期中，同样不要使用this.setState()
     componentDidUpdate() {
         console.log('---------componentDidUpdate');
     }
    
+    /* 销毁阶段（1） */
     // 相当于Vue中的beforeDistory()
+    // 关闭长连接、定时器、清缓存
     componentWillUnmount() {
         console.log('---------componentWillUnmount');
     }
